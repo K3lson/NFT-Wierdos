bfe108cb5c1566e8600434040624a040da0c5e14
Kelvin <n3lsonfree@gmail.com>
2022-04-05 15:01:01 -0700

    Configure React app for deployment to GitHub Pages

---
+++ src/styles/Themes.js
@@ -0,0 +1,53 @@
+//This file contains variables for different themes
+
+import React from 'react'
+
+export const light = {
+    body: "#fff",
+    text: "#202020", //black shade
+    bodyRgba : "255, 255, 255",
+    textRgba: "32, 32, 32",
+
+    carouselColor: "#eeedde",
+
+    fontxs: '0.75em',
+    fontsm: '0.875em',
+    fontmd: '1em', //1em=16px
+    fontlg: '1.25em',
+    fontx1: '2em',
+    fontxx1: '3em',
+    fontxxx1: '4em',
+
+    fontButton: '0.875em',
+
+
+    navHeight: '5rem',
+}
+
+
+export const dark = {
+    body: "#202020",
+    text: "#fff", //black shade
+    bodyRgba : "32, 32, 32",
+    textRgba:  "255, 255, 255",
+
+    carouselColor: "#eeedde",
+
+    fontxs: '0.75em',
+    fontsm: '0.875em',
+    fontmd: '1em', //1em=16px
+    fontlg: '1.25em',
+    fontx1: '2em',
+    fontxx1: '3em',
+    fontxxx1: '4em',
+
+    fontButton: '0.875em',
+
+
+    navHeight: '5rem',
+}
+
+
+
+
+
